6abc7f97a0c8fa46ed9318c6cbcda690e95ee9e8
Gabriel <2164584@claurendeau.qc.ca>
2024-03-05 15:36:08 -0500

    start

---
+++ src/main/java/com/example/projetsynthese/ProjetsyntheseApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetsynthese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetsyntheseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetsyntheseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projetsynthese/ProjetsyntheseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetsynthese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetsyntheseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
