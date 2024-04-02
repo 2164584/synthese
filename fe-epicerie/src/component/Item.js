bfeec0ac90529556d06c4760e4425907814299b5
Gabriel <2164584@claurendeau.qc.ca>
2024-04-02 11:34:45 -0400

    layout des items est correct

--- fe-epicerie/src/component/Item.js
+++ fe-epicerie/src/component/Item.js
@@ -1,7 +1,14 @@
-function Item() {
+function Item({item, index}) {
     return (
-        <div>
-
+        <div key={index} className="col-3 mb-2">
+            <div className="p-2 border border-2 rounded">
+                <img src={item.image} alt={item.name} className="w-100"/>
+                <div>
+                    <h3>{item.name}</h3>
+                    <p>{item.brand}</p>
+                    <p>{item.price}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- fe-epicerie/src/component/ItemList.jsx
+++ fe-epicerie/src/component/ItemList.jsx
@@ -1,24 +1,18 @@
+import Item from "./Item";
+
 function ItemList({itemList}) {
     console.log(itemList)
   return (
-    <div className='row'>
-      <div className="col-12">
-        <h3>Items</h3>
-        <div className="d-flex">
-
-            {itemList.slice(0, 30).map((item, index) => (
-                    <div key={index} className="col-3 mx-3 border border-2 rounded">
-                        <img src={item.image} alt={item.name} className="img-fluid"/>
-                        <div>
-                            <h3>{item.name}</h3>
-                            <p>{item.brand}</p>
-                            <p>{item.price}</p>
-                        </div>
-                    </div>
-            ))}
-        </div>
+      <div className='row'>
+          <div className="col-12">
+              <h3>Items</h3>
+              <div className="row">
+                  {itemList.slice(0, 100).map((item, index) =>
+                      <Item index={index} item={item}/>
+                  )}
+              </div>
+          </div>
       </div>
-    </div>
   );
 }
 
